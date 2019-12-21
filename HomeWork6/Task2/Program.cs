765684b4c77872ce11118bb347d97f1cc70581ef
Alexander Diachenko <alexander.diachenko@creatio.com>
2019-12-21 17:11:38 +0200

    add task 2 to HW6

--- HomeWork6/Task2/Program.cs
+++ HomeWork6/Task2/Program.cs
@@ -10,23 +10,28 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-           /*Задание 2
-Используя Visual Studio, создайте проект по шаблону Console Application.
-Дано два числа A и B(A<B) выведите суму всех чисел расположенных между данными числами на экран.
-Дано два числа A и B(A<B) выведите все нечетные значения, расположенные между данными числами.
-*/
+            /*Задание 2
+ Используя Visual Studio, создайте проект по шаблону Console Application.
+ Дано два числа A и B(A<B) выведите суму всех чисел расположенных между данными числами на экран.
+ Дано два числа A и B(A<B) выведите все нечетные значения, расположенные между данными числами.
+ */
             Console.WriteLine("Enter number A");
             int a = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter number B");
             int b = int.Parse(Console.ReadLine());
-
+           
             int amount = 0;
 
-            for (int i = a; a < b; i++)
-            {                
-                Console.WriteLine($"Total amoun between A and B is {amount += amount}");
+            for (int i = a; i < b; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine($"Not even number {i}");
+                }
+                amount += i;
             }
-
+            
+            Console.WriteLine($"Total amount between {a} and {b} is {amount}");
 
         }
     }
