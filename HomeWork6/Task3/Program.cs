9f17136c5119d464f8a299326f7c3ce2cbda6831
Alexander Diachenko <alexander.diachenko@creatio.com>
2019-12-23 17:55:55 +0200

    add task 2 to HW6

---
+++ HomeWork6/Task3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // прямоугольник
+            Console.WriteLine("Прямоугольник");
+            for (int i = 0; i < 4; i++)
+            {
+                //Console.WriteLine("\n");
+                for (int j = 0; j < 20; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine();
+
+            // прямоугольный треугольник
+            Console.WriteLine("Прямоугольный треугольник");
+            for (int i = 0; i < 10; i++)
+            {                
+                for (int j = i; j < 10; j++)
+                {
+                    Console.Write("*");                    
+                }
+                Console.Write("\n");               
+            }
+
+            //равносторонний треугольник
+            Console.WriteLine();
+            Console.WriteLine("Равносторонний треугольник");
+
+            int spaceTriangle = 10;
+           
+            for (int i = 0; i < 10; i++)
+            {
+                
+                for (int j = spaceTriangle; j  >= 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                
+                spaceTriangle--;
+
+
+                for (int k = 0; k <= (i*2); k++)
+                {
+                    Console.Write("*");                    
+                }
+                Console.Write("\n");
+               
+            }
+
+            //ромб
+            Console.WriteLine();
+            Console.WriteLine("Ромб");
+
+            int spaceRomb = 10;
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                for (int j = spaceRomb; j >= 0; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                spaceRomb--;
+
+
+                for (int k = 0; k <= (i * 2); k++)
+                {
+                    Console.Write("*");                    
+                }
+                Console.Write("\n");
+            }
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                
+                for (int j = 0; j < spaceRomb; j++)
+                {
+                    Console.Write(" ");
+                }
+
+
+                for (int k = 0; k < 20; k++)
+                {
+                    Console.Write("*");                    
+                }
+
+
+
+                
+
+                Console.Write("\n");
+            }
+
+            
+            
+        }
+
+    }
+}
