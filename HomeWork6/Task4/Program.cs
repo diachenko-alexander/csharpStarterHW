3e34785b79d679531fe06a3984ccca10ee27554c
Alexander Diachenko <alexander.diachenko@creatio.com>
2019-12-24 17:17:03 +0200

    add task4

--- HomeWork6/Task4/Program.cs
+++ HomeWork6/Task4/Program.cs
@@ -10,6 +10,26 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Программа расчета вариантов доставки");
+            Console.WriteLine("Введите количество клиентов");
+            int clients = int.Parse(Console.ReadLine());
+            int cars = 1;
+            string message = "Количество вариантов доставки = {0}";
+
+            if (clients == 1)
+            {
+                Console.WriteLine(string.Format(message, clients));
+            } else
+            {
+                do
+                {
+                    cars *= clients--;
+                } while (clients > 0);
+
+                Console.WriteLine(string.Format(message, cars));
+            }
+
+            
         }
     }
 }
