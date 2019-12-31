c3efcce9a9e7b870cf47d1bd488734662b043ce3
a.diachenko <alexdgx@gmail.com>
2019-12-31 20:21:50 +0200

    add HW8

---
+++ HomeWork8/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter pay: ");
+            double pay = double.Parse(Console.ReadLine());
+            CheckPay(pay);
+        }
+
+        static void CheckPay(double pay)
+        {
+            if (pay == 700)
+            {
+                Console.WriteLine("Your credit is finished.");
+            }
+            else if (pay < 700)
+            {
+                Console.WriteLine($"You have to pay {700 - pay}");
+            }
+            else
+            {
+                Console.WriteLine($"Over pay is {pay - 700}");
+            }
+        }
+    }
+}
---
+++ HomeWork8/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter clients:");
+            int routes = CountRoutes(int.Parse(Console.ReadLine()));
+            Console.WriteLine(routes);
+        }
+
+        static int CountRoutes(int clients)
+        {
+
+            if (clients == 0)
+            {
+                return 1;
+            }
+            else
+            {
+               return  clients * CountRoutes (clients -1);
+            }               
+
+        }
+    }
+}
