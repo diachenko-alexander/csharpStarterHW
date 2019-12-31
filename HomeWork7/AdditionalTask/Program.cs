6923707843aa71cfb959278c72eac967a47c2c23
a.diachenko <alexdgx@gmail.com>
2019-12-31 18:48:38 +0200

    add Additional task to HW7

---
+++ HomeWork7/AdditionalTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate(10, 10, 10);
+        }
+
+        static void Calculate (params int [] array)
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+            }
+            Console.WriteLine(result / array.Length);
+
+        }
+    }
+}
